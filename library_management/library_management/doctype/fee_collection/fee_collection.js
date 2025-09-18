862ca99b21b69f5922d43b42e7e2f9dd4ff13cc3
Smit Vora <smitvora203@gmail.com>
2025-09-19 01:03:07 +0530

    feat: fees collection

--- library_management/library_management/doctype/fee_collection/fee_collection.js
+++ library_management/library_management/doctype/fee_collection/fee_collection.js
@@ -1,6 +1,93 @@
 // Fee Collection Client Script
 // This script provides interactive functionality for the Fee Collection doctype
 
-// frappe.ui.form.on("Fee Collection", {
-// 	refresh: function (frm) {},
-// });
+frappe.ui.form.on("Fee Collection", {
+	refresh: function (frm) {
+		// Add custom buttons
+		if (frm.doc.docstatus === 0 && frm.doc.member) {
+			frm.add_custom_button(__("Load Outstanding Fines"), function () {
+				frm.call("load_outstanding_fines").then(() => {
+					frm.refresh_field("fine_details");
+					frm.refresh_field("fine_amount");
+					frm.refresh_field("total_amount");
+					frm.refresh_field("net_amount");
+				});
+			});
+		}
+
+		// Print receipt button
+		if (frm.doc.docstatus === 1) {
+			frm.add_custom_button(__("Print Receipt"), function () {
+				frappe.route_options = { name: frm.doc.name };
+				frappe.set_route("print", "Fee Collection", frm.doc.name);
+			});
+		}
+	},
+
+	member: function (frm) {
+		// Clear fine details when member changes
+		if (frm.doc.member) {
+			frm.set_value("fine_details", []);
+			frm.refresh_field("fine_details");
+		}
+	},
+
+	membership_fee: function (frm) {
+		calculate_total_amount(frm);
+	},
+
+	late_fee: function (frm) {
+		calculate_total_amount(frm);
+	},
+
+	damage_fee: function (frm) {
+		calculate_total_amount(frm);
+	},
+
+	other_fee: function (frm) {
+		calculate_total_amount(frm);
+	},
+
+	fine_amount: function (frm) {
+		calculate_total_amount(frm);
+	},
+
+	discount_amount: function (frm) {
+		calculate_total_amount(frm);
+	},
+});
+
+function calculate_total_amount(frm) {
+	var total =
+		(frm.doc.membership_fee || 0) +
+		(frm.doc.late_fee || 0) +
+		(frm.doc.damage_fee || 0) +
+		(frm.doc.other_fee || 0) +
+		(frm.doc.fine_amount || 0);
+
+	frm.set_value("total_amount", total);
+	frm.set_value("net_amount", total - (frm.doc.discount_amount || 0));
+}
+
+// Child table events
+frappe.ui.form.on("Fee Collection Fine Detail", {
+	fine_details_add: function (frm, cdt, cdn) {
+		// Auto-calculate fine amount when new row is added
+		calculate_fine_total(frm);
+	},
+
+	fine_details_remove: function (frm) {
+		// Recalculate when row is removed
+		calculate_fine_total(frm);
+	},
+});
+
+function calculate_fine_total(frm) {
+	var total_fine = 0;
+	frm.doc.fine_details.forEach(function (row) {
+		total_fine += row.fine_amount || 0;
+	});
+
+	frm.set_value("fine_amount", total_fine);
+	calculate_total_amount(frm);
+}
--- library_management/library_management/doctype/fee_collection/fee_collection.py
+++ library_management/library_management/doctype/fee_collection/fee_collection.py
@@ -40,4 +40,138 @@ class FeeCollection(Document):
 		remarks: DF.Text | None
 		total_amount: DF.Currency
 	# end: auto-generated types
-	pass
+
+	def validate(self):
+		self.calculate_total_amount()
+		self.validate_member_exists()
+
+	def before_submit(self):
+		self.set_collected_by()
+		self.validate_payment_details()
+
+	def on_submit(self):
+		self.update_member_fines()
+		self.update_transaction_fines()
+
+	def on_cancel(self):
+		self.reverse_member_fines()
+		self.reverse_transaction_fines()
+
+	def calculate_total_amount(self):
+		"""Calculate total amount from individual fee components"""
+		total = (
+			(self.membership_fee or 0)
+			+ (self.late_fee or 0)
+			+ (self.damage_fee or 0)
+			+ (self.other_fee or 0)
+			+ (self.fine_amount or 0)
+		)
+
+		self.net_amount = total - (self.discount_amount or 0)
+		self.total_amount = total
+
+	def validate_member_exists(self):
+		"""Validate if member exists and is active"""
+		if not frappe.db.exists("Library Member", self.member):
+			frappe.throw(_("Library Member {0} does not exist").format(self.member))
+
+		member_status = frappe.db.get_value("Library Member", self.member, "membership_status")
+		if member_status == "Cancelled":
+			frappe.throw(_("Cannot collect fees for cancelled member {0}").format(self.member))
+
+	def validate_payment_details(self):
+		"""Validate payment method specific details"""
+		if self.payment_method in ["Card", "UPI", "Net Banking", "Cheque", "Demand Draft"]:
+			if not self.reference_number:
+				frappe.throw(_("Reference Number is required for {0} payment").format(self.payment_method))
+
+		if self.net_amount <= 0:
+			frappe.throw(_("Net amount must be greater than zero"))
+
+	def set_collected_by(self):
+		"""Set the user who collected the payment"""
+		if not self.collected_by:
+			self.collected_by = frappe.session.user
+
+	def update_member_fines(self):
+		"""Update member's total fines after payment"""
+		if self.payment_type == "Fine Payment" and self.fine_amount > 0:
+			member_doc = frappe.get_doc("Library Member", self.member)
+			member_doc.update_total_fines()
+			member_doc.save(ignore_permissions=True)
+
+	def update_transaction_fines(self):
+		"""Mark related transaction fines as paid"""
+		if self.payment_type == "Fine Payment":
+			for fine_detail in self.fine_details:
+				if fine_detail.transaction_id:
+					frappe.db.set_value("Book Transaction", fine_detail.transaction_id, "fine_paid", 1)
+
+	def reverse_member_fines(self):
+		"""Reverse member fines on cancellation"""
+		if self.payment_type == "Fine Payment":
+			member_doc = frappe.get_doc("Library Member", self.member)
+			member_doc.update_total_fines()
+			member_doc.save(ignore_permissions=True)
+
+	def reverse_transaction_fines(self):
+		"""Reverse transaction fine payments on cancellation"""
+		if self.payment_type == "Fine Payment":
+			for fine_detail in self.fine_details:
+				if fine_detail.transaction_id:
+					frappe.db.set_value("Book Transaction", fine_detail.transaction_id, "fine_paid", 0)
+
+	@frappe.whitelist()
+	def get_outstanding_fines(self):
+		"""Get all outstanding fines for the member"""
+		if not self.member:
+			return []
+
+		outstanding_fines = frappe.db.sql(
+			"""
+			SELECT
+				name as transaction_id,
+				book,
+				fine_amount,
+				transaction_date,
+				due_date,
+				return_date
+			FROM `tabBook Transaction`
+			WHERE member = %s
+			AND docstatus = 1
+			AND fine_amount > 0
+			AND fine_paid = 0
+			ORDER BY transaction_date DESC
+		""",
+			(self.member,),
+			as_dict=1,
+		)
+
+		return outstanding_fines
+
+	@frappe.whitelist()
+	def load_outstanding_fines(self):
+		"""Load outstanding fines into the fine details table"""
+		outstanding_fines = self.get_outstanding_fines()
+
+		# Clear existing fine details
+		self.set("fine_details", [])
+
+		total_fine_amount = 0
+
+		for fine in outstanding_fines:
+			self.append(
+				"fine_details",
+				{
+					"transaction_id": fine.transaction_id,
+					"book": fine.book,
+					"fine_amount": fine.fine_amount,
+					"transaction_date": fine.transaction_date,
+					"due_date": fine.due_date,
+					"return_date": fine.return_date,
+				},
+			)
+			total_fine_amount += fine.fine_amount
+
+		self.fine_amount = total_fine_amount
+		self.calculate_total_amount()
